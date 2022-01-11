0f412eea430ac57d9a3d6ba644d695755e139455
Nuri Yuri <youri.54@live.fr>
2022-01-11 23:26:08 +0100

    Add update so we can read axis position

--- example/joystick.js
+++ example/joystick.js
@@ -6,6 +6,7 @@ Joystick.update();
 console.log(Joystick.isConnected(0));
 console.log(Joystick.getIdentification(0));
 console.log(Joystick.getButtonCount(0));
+Joystick.update();
 console.log(Object.fromEntries(Object.entries(Joystick.Axis).map(
   ([ key, axis ]) => ([ key, Joystick.hasAxis(0, axis) ]))));
 console.log(Object.fromEntries(Object.entries(Joystick.Axis).map(
